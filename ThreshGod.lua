a8ecd32d6e98cd3c8807a16d8adfddd838039328
estruptum <cheapeloboostreliable@gmail.com>
2015-07-21 16:13:17 +0200

    Update ThreshGod.lua

--- ThreshGod.lua
+++ ThreshGod.lua
@@ -5,33 +5,50 @@ require("IWalk")
  
 AddInfo("Thresh", "Thresh")
 AddButton("Q", "Use Q", true)
-AddButton("E", "Use E", false)  -- false due to inspired.lua bug
+AddButton("W", "use W", true)
+AddButton("E", "Use E", true)
 AddButton("R", "Use R", true)
-function AfterObjectLoopEvent(myHero)
+
+
+
+AddAfterObjectLoopEvent(function(myHero)
+    local Obj_Type = GetObjectType(Object);
+            if Obj_Type == Obj_AI_Hero then
+
     IWalk()
-    DrawMenu()
      if GetKeyValue("Combo") then
         target = GetCurrentTarget()
             if ValidTarget(target, 1100) then
-            local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1900,500,1100,70,true,true)
+            local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1900,500,1100,70,true,true)  -- GetPredictionForPlayer(startPosition, targetUnit, targetUnitMoveSpeed, spellTravelSpeed, spellDelay, spellRange, spellWidth, collision, addHitBox)
                     if GetButtonValue("Q") then
                      if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 then
                      CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
                      end
                     end
+
+                    if GetButtonValue("W") then -- rango 950
+                        if CanUseSpell(myHero,_W) == READY then
+                        CastTargetSpell(Object,_W)
+                        end
+                    end
+
  
                     if GetButtonValue("E") then
-                     if CanUseSpell(myHero, _E) == READY and Epred.HitChange == 1 then
+                     local EPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1875,250,400,55,false,true)
+                     if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 then
                      CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
                      end
                     end
  
            
                     if GetButtonValue("R") then
-                     if CanUseSpell(myHero, _R) == READY and Rpred.HitChange == 1 then
+                    local RPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),0,250,450,55,false,true)
+
+                     if CanUseSpell(myHero, _R) == READY and RPred.HitChance == 1 then
                      CastSkillShot(_R,RPred.PredPos.x,RPred.PredPos.y,RPred.PredPos.z)
                      end
                    end
             end
-     end
-end
+        end
+    end
+end)
