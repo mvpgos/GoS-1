d50ac2e0716ea3e379296af90bd8b28d69cc97bb
estruptum <cheapeloboostreliable@gmail.com>
2015-08-02 21:52:56 +0200

    Update ThreshGod.lua

--- ThreshGod.lua
+++ ThreshGod.lua
@@ -12,13 +12,13 @@ OnLoop(function(myHero)
 	        if ValidTarget(target, 1100) then
 	                	
             local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1900,500,1100,70,true,true)  -- GetPredictionForPlayer(startPosition, targetUnit, targetUnitMoveSpeed, spellTravelSpeed, spellDelay, spellRange, spellWidth, collision, addHitBox)
-                    if GetButtonValue("Q") then
+                    if Config.Q
                      if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 then
                      CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
                      end
                     end
 
-                    if GetButtonValue("W") then -- rango 950
+                    if Config.W then -- rango 950
 					local Ally = GetObjectType(Object);
                      if Ally == Obj_AI_Hero then
                         local WPred = GetPredictionForPlayer(GetMyHeroPos(),ally,GetMoveSpeed(ally),1875,250,800,55,false,true)
@@ -29,7 +29,7 @@ OnLoop(function(myHero)
                     end
 
  
-                    if GetButtonValue("E") then
+                    if Config.E then
                      local EPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1875,250,400,55,false,true)
                      if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 then
                      CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
@@ -37,7 +37,7 @@ OnLoop(function(myHero)
                     end
  
            
-                    if GetButtonValue("R") then
+                    if Config.R then
                     local RPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),0,250,450,55,false,true)
                      if CanUseSpell(myHero, _R) == READY and RPred.HitChance == 1 then
                      CastSkillShot(_R,RPred.PredPos.x,RPred.PredPos.y,RPred.PredPos.z)
