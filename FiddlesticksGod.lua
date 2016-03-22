74edbc267b2552bb1972f2ed5907860789a010a9
estruptum <cheapeloboostreliable@gmail.com>
2016-03-22 19:21:58 +0100

    Update and rename Fiddlesticks.lua to FiddlesticksGod.lua

--- FiddlesticksGod.lua
+++ FiddlesticksGod.lua
@@ -4,7 +4,7 @@ function AutoUpdate(data)
     if tonumber(data) > tonumber(ver) then
         PrintChat("New version found! " .. data)
         PrintChat("Downloading update, please wait...")
-        DownloadFileAsync("https://github.com/estruptum/GoS/blob/master/Fiddlesticks.lua", SCRIPT_PATH .. "Fiddlesticks.lua", function() PrintChat("Update Complete, please 2x F6!") return end)
+        DownloadFileAsync("https://github.com/estruptum/GoS/blob/master/FiddlesticksGod.lua", SCRIPT_PATH .. "FiddlesticksGod.lua", function() PrintChat("Update Complete, please 2x F6!") return end)
     else
         PrintChat("No updates found!")
     end
